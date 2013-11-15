702ac15d69bdb04c64ed6cc0d4958692e375ae5c
Hieu Nguyen <hieu.ngvan@gmail.com>
2013-11-15 01:25:18 -0800

    create GameMetadata

--- src/com/tuongky/GameSearchServlet.java
+++ src/com/tuongky/GameSearchServlet.java
@@ -1,18 +1,32 @@
 package com.tuongky;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.collect.Lists;
+import com.tuongky.backend.GameMetadataDao;
 import com.tuongky.model.GameQuery;
 import com.tuongky.model.GameSearchResult;
+import com.tuongky.model.datastore.Game;
+import com.tuongky.model.datastore.GameMetadata;
 import com.tuongky.service.SearchService;
 import com.tuongky.util.JsonUtils;
 
 @SuppressWarnings("serial")
 public class GameSearchServlet extends HttpServlet {
+
+  private List<GameMetadata> getGameMetadatas(List<Game> games) {
+    GameMetadataDao gameMetadataDao = new GameMetadataDao();
+    List<String> ids = Lists.newArrayList();
+    for (Game game : games) {
+      ids.add(game.getId());
+    }
+    return gameMetadataDao.getByIds(ids);
+  }
 
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -31,6 +45,7 @@ public class GameSearchServlet extends HttpServlet {
     resp.setContentType(Constants.CT_JSON_UTF8);
     resp.getWriter().println(JsonUtils.toJson(
         "games", gameSearchResult.getGames(),
+        "gameMetadatas", getGameMetadatas(gameSearchResult.getGames()),
         "numberFound", gameSearchResult.getNumberFound()
     ));
   }
