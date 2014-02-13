8ecbe6755ca130858a9542fb9b507d90cd86bec4
Hieu Nguyen <hieu.ngvan@gmail.com>
2014-02-13 02:07:07 -0800

    tmp

---
+++ test/com/tuongky/backend/UserDaoTest.java
@@ -0,0 +1,13 @@
+package com.tuongky.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoTest {
+
+  @Test
+  public void test() {
+    assertTrue(1 + 1 == 2);
+  }
+}
